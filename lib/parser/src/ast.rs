2cdb0ddf3cb7bcd23385b4f438207ea892412e28
Joshua Bassin <18689065+jbassin@users.noreply.github.com>
2020-09-30 22:58:39 -0400

    Changed to lalrpop from peg. Simplified AST.

--- lib/parser/src/ast.rs
+++ lib/parser/src/ast.rs
@@ -1,66 +1,105 @@
 use std::fmt;
 
-#[derive(PartialEq)]
-pub enum Literal {
-    Integer(i64),
-    Float(f64),
-    Boolean(bool),
-    Atom(String),
-    Variable(String),
+#[derive(Clone, Debug, PartialEq)]
+pub struct Location {
+    start: usize,
+    end: usize,
 }
 
-impl fmt::Debug for Literal {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        use Literal::*;
-
-        match self {
-            Integer(i) => write!(f, "{:#?}", i),
-            Float(f_) => write!(f, "{:#?}", f_),
-            Boolean(b) => write!(f, "{:#?}", b),
-            Atom(a) => write!(f, "{:#?}", a),
-            Variable(v) => write!(f, "{:#?}", v),
+impl Location {
+    pub fn new(start: usize, end: usize) -> Self {
+        Self {
+            start,
+            end,
         }
     }
 }
 
-#[derive(PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 pub enum Operator {
-    Add,
-    Subtract,
-    Multiply,
-    Divide,
-    Modulo,
-    Custom(String),
+    Add {
+        location: Location,
+        value: String,
+    },
+
+    Sub {
+        location: Location,
+        value: String,
+    },
+
+    Mul {
+        location: Location,
+        value: String,
+    },
+
+    Div {
+        location: Location,
+        value: String,
+    },
+
+    Mod {
+        location: Location,
+        value: String,
+    },
+
+    Pow {
+        location: Location,
+        value: String,
+    },
+
+    Custom {
+        location: Location,
+        value: String,
+    },
 }
 
-impl fmt::Debug for Operator {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+impl Operator {
+    pub fn value(&self) -> &String {
         use Operator::*;
 
         match self {
-            Add => write!(f, "+"),
-            Subtract => write!(f, "-"),
-            Multiply => write!(f, "*"),
-            Divide => write!(f, "/"),
-            Modulo => write!(f, "%"),
-            Custom(s) => write!(f, "{}", s),
+            Add { value, .. } => value,
+            Sub { value, .. } => value,
+            Mul { value, .. } => value,
+            Div { value, .. } => value,
+            Mod { value, .. } => value,
+            Pow { value, .. } => value,
+            Custom { value, .. } => value,
         }
     }
 }
 
-#[derive(PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 pub enum Expression {
-    Binary(Box<Expression>, Operator, Box<Expression>),
-    Literal(Literal),
-}
+    Integer {
+        location: Location,
+        value: String,
+    },
 
-impl fmt::Debug for Expression {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        use Expression::*;
+    Float {
+        location: Location,
+        value: String,
+    },
 
-        match self {
-            Binary(lhs, op, rhs) => write!(f, "({:#?} {:#?} {:#?})", lhs, op, rhs),
-            Literal(l) => write!(f, "{:#?}", l)
-        }
-    }
+    Boolean {
+        location: Location,
+        value: String,
+    },
+
+    Atom {
+        location: Location,
+        value: String,
+    },
+
+    Variable {
+        location: Location,
+        value: String,
+    },
+
+    BinaryOperation {
+        location: Location,
+        operator: Operator,
+        left: Box<Self>,
+        right: Box<Self>,
+    },
 }
--- lib/parser/src/lib.rs
+++ lib/parser/src/lib.rs
@@ -1,10 +1,134 @@
-extern crate pest;
-#[macro_use]
-extern crate pest_derive;
+#[macro_use] extern crate lalrpop_util;
+lalrpop_mod!(pub grammar);
+
+#[macro_use] extern crate lazy_static;
 
 mod ast;
-mod transform;
 
 pub fn test() -> String {
     "hilo".to_owned()
+}
+
+#[cfg(test)]
+mod transform_tests {
+    use super::*;
+    use crate::ast::{Expression, Expression::*, Location, Operator};
+
+    lazy_static! {
+        static ref PARSER: grammar::ExprParser = grammar::ExprParser::new();
+    }
+
+    fn parse(token: &'static str) -> Box<Expression> {
+        PARSER.parse(token).unwrap()
+    }
+
+    #[test]
+    fn literal_integer() {
+        assert_eq!(
+            *parse("4"),
+            Integer {
+                location: Location::new(0, 1),
+                value: "4".to_string()
+            },
+        );
+    }
+
+    #[test]
+    fn literal_float() {
+        assert_eq!(
+            *parse("4."),
+            Float {
+                location: Location::new(0, 2),
+                value: "4.".to_string()
+            },
+        );
+    }
+
+    #[test]
+    fn literal_variable() {
+        assert_eq!(
+            *parse("foo_bar"),
+            Variable {
+                location: Location::new(0, 7),
+                value: "foo_bar".to_string()
+            },
+        );
+    }
+
+    #[test]
+    fn literal_atom() {
+        assert_eq!(
+            *parse(":atom"),
+            Atom {
+                location: Location::new(0, 5),
+                value: "atom".to_string()
+            },
+        );
+    }
+
+    #[test]
+    fn literal_true() {
+        assert_eq!(
+            *parse("(true)"),
+            Boolean {
+                location: Location::new(1, 5),
+                value: "true".to_string()
+            },
+        );
+    }
+
+    #[test]
+    fn literal_false() {
+        assert_eq!(
+            *parse("false"),
+            Boolean {
+                location: Location::new(0, 5),
+                value: "false".to_string()
+            },
+        );
+    }
+
+    #[test]
+    fn bin_op_pow() {
+        assert_eq!(
+            *parse("1 ** 2."),
+            BinaryOperation {
+                location: Location::new(0, 7),
+                operator: Operator::Pow {
+                    location: Location::new(2, 4),
+                    value: "**".to_string()
+                },
+                left: Box::new(Integer {
+                    location: Location::new(0, 1),
+                    value: "1".to_string(),
+                }),
+                right: Box::new(Float {
+                    location: Location::new(5, 7),
+                    value: "2.".to_string(),
+                }),
+            }
+        );
+    }
+
+    #[test]
+    fn bin_op_level_1_custom() {
+        assert_eq!(
+            *parse("1 **> 2."),
+            BinaryOperation {
+                location: Location::new(0, 8),
+                operator: Operator::Custom {
+                    location: Location::new(2, 5),
+                    value: "**>".to_string()
+                },
+                left: Box::new(Integer {
+                    location: Location::new(0, 1),
+                    value: "1".to_string(),
+                }),
+                right: Box::new(Float {
+                    location: Location::new(6, 8),
+                    value: "2.".to_string(),
+                }),
+            }
+        );
+    }
 }
